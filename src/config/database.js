30fa6c058f61ec377dda2c86536a9f92ee99679f
MD TAHSEEN RAZA <114172906+mdtahseenraza@users.noreply.github.com>
2024-12-25 20:37:43 +0530

    Create database.js

---
+++ src/config/database.js
@@ -0,0 +1,14 @@
+const { Sequelize } = require('sequelize');
+require('dotenv').config();
+
+const sequelize = new Sequelize({
+  dialect: 'postgres',
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  logging: false
+});
+
+module.exports = sequelize;
